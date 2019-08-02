98184bb0d81c2d0bf4b191f33f2038521c29a596
Chris Jones <chrisjonesdesign85@gmail.com>
2019-08-02 18:09:47 -0400

    got index.js set up

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require('./server');
+require('dotenv').config();
+
+const port = process.env.PORT ? process.env.PORT : 4000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on port ${port} ***\n`);
+});
